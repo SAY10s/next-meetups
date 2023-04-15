83fcf478ef381b9754e4827a2462011e1a915b82
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-15 10:40:58 +0200

    ADD: new meetup

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,15 @@
+import Layout from "@/components/layout/Layout";
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
   function addMeetupHandler(enteredMeetupData) {
     console.log(enteredMeetupData);
   }
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Layout>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Layout>
+  );
 };
 
 export default NewMeetupPage;
