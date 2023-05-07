69bb083f7cb3b95913c42f3d9751f698cb88c4f1
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-05-07 19:42:58 +0200

    ADD: i just don't know what to do with myself

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,9 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { useRouter } from "next/router";
 
 const MeetupDetails = () => {
+  useRouter;
+
   return (
     <MeetupDetail
       image="https://www.ashleyabroad.com/wp-content/uploads/2019/10/Marienplatz-Munich-Germany-1.jpg"
@@ -10,5 +13,21 @@ const MeetupDetails = () => {
     />
   );
 };
+
+export async function getStaticProps() {
+  //fetch for a single meetup
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://www.ashleyabroad.com/wp-content/uploads/2019/10/Marienplatz-Munich-Germany-1.jpg",
+        id: "m1",
+        title: "spotkanie",
+        address: "Gołkowice XD",
+        description: "Opis miejsca",
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
